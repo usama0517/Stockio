823e69bc021b0c56e01f0f4c8640b44b36e42ae9
Usama Abdurahman <usamaabdurahman05@gmail.com>
2025-04-18 21:52:21 +0300

    My Second commit

---
+++ backend/src/main/java/com/project/Stokio/StokioApplication.java
@@ -0,0 +1,13 @@
+package com.project.Stokio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StokioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StokioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/Stokio/StokioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Stokio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StokioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
